fe1271a715d8b46050744b6c666bdf0a5aa8106f
Gabriel Garcia <devgglop@gmail.com>
2023-07-30 22:16:12 +0200

    Basic config

---
+++ app/src/main/java/com/gabr/gabc/qook/presentation/HomeScreen.kt
@@ -0,0 +1,19 @@
+package com.gabr.gabc.qook.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import com.gabr.gabc.qook.R
+import com.gabr.gabc.qook.presentation.theme.AppTheme
+
+class HomeScreen : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                Text(getString(R.string.app_name))
+            }
+        }
+    }
+}
